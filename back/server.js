f3c6065d81dba6101e96a27ffe7a7f8bc8ea2763
Ariel Tedgui <atedgui98@gmail.com>
2021-01-03 17:40:49 +0100

    Update

--- back/server.js
+++ back/server.js
@@ -3,25 +3,41 @@ const express = require('express');
 const helmet = require('helmet');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const tunnel = require('tunnel-ssh');
 
 dotenv.config();
-const PORT = process.env.PORT || 9292;
 const DB_CONNECT = process.env.DB_CONNECT;
 
 const app = express();
 
-/*mongoose
-	.connect(
-		DB_CONNECT,
-		{ useNewUrlParser: true, useUnifiedTopology: true }
-	)
-	.then(() => {
-		console.log("Connected to DB.");
-	})
-	.catch((err) => {
-		console.log("Error while DB connecting.");
-		console.log(err);
-	});*/
+const config = {
+  username: process.env.USERNAME,
+  host: process.env.HOST,
+  agent: process.env.SSH_AUTH_SOCK,
+  port: process.env.PORT,
+  dstHost: process.env.DSTHOST,
+  dstPort: process.env.DSTPORT,
+  password: process.env.PASSWORD
+};
+
+tunnel(config, (error) => {
+  if (error) {
+    console.log("SSH connection error: " + error);
+  }
+
+  mongoose
+    .connect(
+      DB_CONNECT,
+      { useNewUrlParser: true, useUnifiedTopology: false }
+    )
+    .then(() => {
+      console.log("Connected to DB.");
+    })
+    .catch((err) => {
+      console.log("Error while DB connecting.");
+      console.log(err);
+    });
+});
 
 app.use(express.json());
 app.set('json spaces', 2);
@@ -30,4 +46,4 @@ app.use(helmet());
 
 app.options('*', cors());
 
-app.listen(PORT, () => console.log(`📡 Running on port ${PORT}`));
+app.listen(9292, () => console.log(`📡 Running on port 9292`));
