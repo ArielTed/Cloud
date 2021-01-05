52b058892853a113e6a2b5fd35ed9d8ec53c2402
Ali <aliyoussef34@hotmail.fr>
2021-01-05 09:35:33 +0100

    Dev queries finished, working on user queries

--- front/src/Components/HeaderNav/HeaderNav.js
+++ front/src/Components/HeaderNav/HeaderNav.js
@@ -7,8 +7,9 @@ function HeaderNav() {
   return (
     <div className="HeaderNav">
       <div className="HeaderNav_itemsContainer">
-        <Link to="/user-queries" className="HeaderNav_navItems">User</Link>
-        <Link to="/dev-queries" className="HeaderNav_navItems">Developper</Link>
+        <Link to="/" className="HeaderNav_navItems">Home</Link>
+        {/* <Link to="/user-queries" className="HeaderNav_navItems">User</Link>
+        <Link to="/dev-queries" className="HeaderNav_navItems">Developper</Link> */}
         {/* <Link to="/admin-panel" className="HeaderNav_navItems">Admin</Link> */}
       </div>
       
